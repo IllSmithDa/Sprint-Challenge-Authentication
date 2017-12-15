a89881018a93b2c7fa62fdb80ce922247afb2fe8
Sam Kim <sams404link@gmail.com>
2017-12-15 10:30:22 -0800

    updated challenge

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,7 +4,19 @@ const bcrypt = require('bcrypt');
 const createUser = (req, res) => {
   // there should be a user object set on req
   // use that req.user object to create a user and save it to our Mongo instance.
-};
+  const { username }  = req.body;
+  const password = req.password;
+  const newUser = new User({ username, password });
+  console.log('Hello');
+  newUser.save((err, savedUser) => {
+    if (err) {
+      res.status(422).json({ 'Need both username and password': err.message })
+      return;
+    };
+    res.status(200).json(savedUser);
+  });
+
+};    
 
 module.exports = {
   createUser
--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -27,6 +27,19 @@ const encryptUserPW = (req, res, next) => {
   // Once the password is encrypted using bcrypt, you'll need to save the user the DB.
   // Once the user is set, take the savedUser and set the returned document from Mongo on req.user
   // call next to head back into the route handler for encryptUserPW
+  if (!password) {
+    res.status(422).json('Error: must provide a password');
+    return;
+  };
+  bcrypt
+    .hash(password, SaltRounds)
+    .then(pw => {
+      req.password = pw;
+      next();
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
 };
 
 const compareUserPW = (req, res, next) => {
@@ -36,6 +49,27 @@ const compareUserPW = (req, res, next) => {
   // You'll need to find the user in your DB
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
   // If the passwords match set the username on `req` ==> req.username = user.username; and call next();
+  if (!username) {
+    res.status(422).json('Error: Must provide a username');
+  };
+  User.findOne({ username }, (err, user) => {
+    if (user || err === null) {
+      res.status(422).json('username does not exist');
+    };
+    const hashedPw = user.password;
+    bcrypt
+      .compare(password, hashedPw)
+      .then(response => {
+        if (!response ) {
+          throw new Error(err);
+        };
+        req.username = user.username;
+        next();
+      })
+      .catch(err2 => {
+        res.status(422).json('username/password does not match');
+      });
+  });
 };
 
 module.exports = {
